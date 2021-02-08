a773267fff2caf91d56ad6437cc028e6329f926c
doepc <hayrigomer@gmail.com>
2021-02-07 21:19:17 -0800

    Some resources

--- main.cpp
+++ main.cpp
@@ -3,44 +3,44 @@
 ********************************************************/
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <ctime>
 #include <string>
 
 const std::string ResourcePath = "res/";
+using namespace sf;
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "DogsnCats", sf::Style::Close | sf::Style::Resize);
-
-	sf::RectangleShape background(sf::Vector2f(1920.0f, 1080.0f));
-	sf::Texture backgroundTexture;
-	sf::Vector2f BackgroundSize = background.getSize();
+	RenderWindow window(VideoMode(1280, 720), "DogsnCats", Style::Close | Style::Resize);
+	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(60);
+	Texture backgroundTexture;
 	backgroundTexture.loadFromFile(ResourcePath + "images/doge.png");
-	background.setTexture(&backgroundTexture);
+	Sprite bgSprite;
+	bgSprite.setPosition((window.getSize().x / 2)-(backgroundTexture.getSize().x / 2),(window.getSize().y / 2)- (backgroundTexture.getSize().y /2));
+	bgSprite.setTexture(backgroundTexture);
 
 	while (window.isOpen()) {
-		sf::Event event;
+		Event event{};
 		while (window.pollEvent(event)) {
 			switch (event.type) {
-				case sf::Event::Closed:
+				case Event::Closed:
 					window.close();
 					break;
-				case sf::Event::MouseButtonPressed:  // move star with mouse
+				case Event::MouseButtonPressed:  // move star with mouse
 					break;
-				case sf::Event::KeyPressed:
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::M));
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::C));
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right));
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left));
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up));
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down));
+				case Event::KeyPressed:
+					if (Keyboard::isKeyPressed(Keyboard::M));
+					if (Keyboard::isKeyPressed(Keyboard::C));
+					if (Keyboard::isKeyPressed(Keyboard::Right));
+					if (Keyboard::isKeyPressed(Keyboard::Left));
+					if (Keyboard::isKeyPressed(Keyboard::Up));
+					if (Keyboard::isKeyPressed(Keyboard::Down));
 					break;
 				default:;
 			}
 		}
+		window.clear();
+		window.draw(bgSprite);
 		window.display();
 	}
 	return 0;
