9bb34cb324c13e2c4dc3b80a760bfe04c6e3f317
Tommy Vu <tommyvu008@yahoo.com>
2021-03-11 22:50:15 -0800

    Testing a comment

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Collider.h"
 
+//Heyo!!
 ostream& operator <<(ostream& os, const Collider& collider) {
 	os << collider.typeName() << " :" << /*"uid: " << collider.uid <<*/  " health: "
 		<< collider.health << " speed: " << collider.speed
