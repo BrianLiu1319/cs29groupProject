70f28cb33355cb070825580ad3121c9eb65ab9f3
doepc <hayrigomer@gmail.com>
2021-03-13 00:13:35 -0800

    fixup! Documentation for Collider

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -3,7 +3,6 @@
 Inventory::Inventory(Vector2f pt, int co, const Texture &texture) {
 	invSprite.setTexture(texture);
 	invSprite.setPosition(pt);
-	invSprite.setTextureRect(IntRect(0, 0, 54, 54));
-//	contain = co;
-	invSprite.setScale(Vector2f(0.5, 0.5));
+	contain = co;
+	invSprite.setScale(Vector2f(0.15, 0.15));
 }
